39d5c2203d21d3b8c1819ef267cef13c6b903b4b
itsBachi <marvin_silvala@dlsu.edu.ph>
2023-03-15 01:19:09 +0800

    Added new routes with the new functions

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\DeliveriesController;
+use App\Http\Controllers\TransactionsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +15,12 @@ Route::group(['prefix' => 'v1'], function () {
     Route::post('/products-create', [ProductsController::class, 'create']);
     Route::put('/products-update/{id}', [ProductsController::class, 'update']);
     Route::delete('/products-delete/{id}', [ProductsController::class, 'delete']);
+
+    Route::get('/deliveries-collection', [DeliveriesController::class, 'getAllPaginated']);
+    Route::get('/deliveries-update/{id}', [DeliveriesController::class, 'update']);
+    Route::get('/deliveries-delete{id}', [DeliveriesController::class, 'delete']);
+
+    Route::get('/transactions-collection', [TransactionsController::class, 'getAllPaginated']);
+    Route::get('/transactions-update/{id}', [TransactionsController::class, 'update']);
+    Route::get('/transactions-delete{id}', [TransactionsController::class, 'delete']);
 });
